c73c7775152e1dcfc42e21d31302f40cc4a5389b
AzadehJafari <jafari.azadeh2018@gmail.com>
2022-05-05 07:33:22 +0430

    HW6

--- 01.js
+++ 01.js
@@ -3,7 +3,6 @@
 // Create a function that takes in an array and returns an array of the accumulating
 // sum:
 
-
 function accSumArr(numArr) {
     let result = 0;
     return numArr.map(x => result += x);
